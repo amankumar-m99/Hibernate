7cbd0976ffa6df4952bda10b71aa5d94eb50fe1d
Aman Kumar <amankumar.m99@gmail.com>
2024-11-22 00:22:12 +0530

    updated syntax of User entity

--- src/entity/e001/User.java
+++ src/entity/e001/User.java
@@ -12,7 +12,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 
-@Entity()
+@Entity
 //@Table(name = "user_table")
 public class User {
 	@Id
